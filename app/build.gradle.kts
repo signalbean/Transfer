6127abbba9ee7ec5c2cf1d91a9f27b0d3416b418
matan h <matan.honig2@gmail.com>
2025-07-02 23:07:06 +0300

    v0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.matanh.transfer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 302
-        versionName = "0.3.2"
+        versionCode = 303
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
